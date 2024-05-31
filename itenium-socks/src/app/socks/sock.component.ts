32d67f35b0a9bed0949166a360918b3cf11c0d10
Tim Bogaert <tim.bogaert.92@gmail.com>
2024-05-31 14:54:31 +0200

    Sock detail page: navigate to next sock

--- itenium-socks/src/app/socks/sock.component.ts
+++ itenium-socks/src/app/socks/sock.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Sock } from './sock.model';
 import { SocksService } from './socks.service';
 import { AsyncPipe, NgIf, TitleCasePipe } from '@angular/common';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
   selector: 'app-sock',
@@ -11,17 +11,20 @@ import { ActivatedRoute } from "@angular/router";
   imports: [NgIf, AsyncPipe, TitleCasePipe],
   templateUrl: './sock.component.html'
 })
-export class SockComponent {
+export class SockComponent implements OnInit {
   sockId: number = -1;
   sock$!: Observable<Sock>;
 
   constructor(private socksService: SocksService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit(): void {
-    this.sockId = this.route.snapshot.params['id'];
-    this.sock$ = this.socksService.getById(this.sockId);
+    this.route.url.subscribe(url => {
+      this.sockId = this.route.snapshot.params['id'];
+      this.sock$ = this.socksService.getById(this.sockId);
+    })
   }
 
   buy(): void {
@@ -31,5 +34,10 @@ export class SockComponent {
   addReview(): void {
     // TODO: Bind the form!
     this.socksService.addReview(this.sockId, 'my review', 5).subscribe();
+  }
+
+  goToNextSock() {
+    let nextSockId = +this.sockId + 1;
+    this.router.navigate(["/socks", nextSockId]);
   }
 }
