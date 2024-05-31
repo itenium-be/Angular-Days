04f552629d733613c0765f8b95602fe5dfccec35
ymanfroy <yannick.manfroy@telenetgroup.be>
2024-05-31 16:29:33 +0200

    Routing - active link

--- itenium-socks/src/app/layout/header.component.ts
+++ itenium-socks/src/app/layout/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {}
