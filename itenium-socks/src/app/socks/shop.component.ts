25dbb00ba51e49e1f7c9108bb8f751f28a180f64
Tim Bogaert <tim.bogaert.92@gmail.com>
2024-05-31 13:25:50 +0200

    Add pagination to socks on shop page

--- itenium-socks/src/app/socks/shop.component.ts
+++ itenium-socks/src/app/socks/shop.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { SocksService } from './socks.service';
-import { Observable } from 'rxjs';
+import {last, Observable} from 'rxjs';
 import { Sock } from './sock.model';
 import { AsyncPipe, NgFor } from '@angular/common';
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [NgFor, AsyncPipe],
+  imports: [NgFor, AsyncPipe, MatPaginator],
   templateUrl: './shop.component.html'
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit{
+  totalItems = 100;
+  pageSize = 8;
+  currentPage = 0;
   socks$!: Observable<Sock[]>;
+  socks: Sock[] = [];
 
   constructor(private socksService: SocksService) {}
 
   ngOnInit(): void {
     this.socks$ = this.socksService.get();
+    this.socks$.pipe(last())
+      .subscribe(allSocks => {
+        this.socks = this.getSocksPage(allSocks, this.currentPage, this.pageSize)
+        this.totalItems = allSocks.length
+      })
+  }
+
+  pageChanged(event: PageEvent) {
+    this.currentPage = event.pageIndex;
+    this.socks$.pipe(last())
+      .subscribe(allSocks => {
+        this.socks = this.getSocksPage(allSocks, this.currentPage, this.pageSize)
+      })
+  }
+
+  private getSocksPage(allSocks: Sock[], currentPage: number, pageSize: number) {
+    const index = currentPage * pageSize
+    return allSocks.slice(index, index + pageSize);
   }
 }
